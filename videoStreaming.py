de375fe87a4d9938cb0a47bbf430d713974b6e90
ankush1012 <ankush_80@outlook.com>
2019-07-28 18:59:39 -0400

    Add files via upload

---
+++ videoStreaming.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+
+# Import packages
+import cv2
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+
+print("Press q to switch off Video Camera")
+# Set up camera constants
+#IM_WIDTH = 1280
+#IM_HEIGHT = 720
+IM_WIDTH = 640    #Use smaller resolution for
+IM_HEIGHT = 480   #slightly faster framerate
+
+# Initialize Picamera and grab reference to the raw capture
+camera = PiCamera()
+camera.resolution = (IM_WIDTH,IM_HEIGHT)
+camera.rotation = 180
+camera.framerate = 10
+rawCapture = PiRGBArray(camera, size=(IM_WIDTH,IM_HEIGHT))
+rawCapture.truncate(0)
+
+for frame in camera.capture_continuous(rawCapture, format="bgr",use_video_port=True):
+    image = frame.array
+    cv2.imshow("Frame", image)
+
+# Press 'q' to quit
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+    rawCapture.truncate(0)
+
+camera.close()
+
+cv2.destroyAllWindows()
+
